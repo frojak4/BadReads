e089ae75bb0faee55a9d7d68adb1cbc18a53736e
frojak4 <Frojak4@gmail.com>
2024-05-24 12:25:31 +0200

    Initial commit

---
+++ GoodReads/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoodReads
+{
+    internal class Book
+    {
+        public string Title;
+        public string Author;
+        public int Pages;
+
+
+        public Book(string title, string author, int pages)
+        {
+            Title = title;
+            Author = author;
+            Pages = pages;
+        }
+    }
+}
---
+++ GoodReads/User.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoodReads
+{
+    internal class User
+    {
+        public string Name;
+        public int BookGoal;
+        public List<Book> Books;
+
+        public User(string name, int bookgoal, List<Book> books)
+        {
+            Name = name;
+            BookGoal = bookgoal;
+            Books = books;
+        }
+
+        public int countBooks()
+        {
+            int count = 0;
+            foreach (var book in Books)
+            {
+                count++;
+            }
+
+            return count;
+        }
+
+        public int countPages()
+        {
+            int count = 0;
+            foreach (var book in Books)
+            {
+                count += book.Pages;
+            }
+
+            return count;
+        }
+    }
+}
